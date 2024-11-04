8b5b23c1d0a5bf15b12a9ecbd4cbfe5e96cd6b91
BSugarSweet <st432320@gmail.com>
2024-11-04 18:39:51 +0800

    Two Pointers Linked List Easy

---
+++ 160. Intersection of Two Linked Lists.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        first = headA
+        second = headB
+        while first != second:
+            first = first.next if first != None else headB
+            second = second.next if second != None else headA
+        return first
+        
